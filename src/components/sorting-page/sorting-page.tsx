893ab8e6efc5f9e83615c2387f78b41cf4dd8d1c
annasilina <92690224+annasilina@users.noreply.github.com>
2023-03-07 17:01:10 +0300

    add visual content for sorting page added randomArr func

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,73 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, {useEffect, useMemo, useState} from "react";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import styles from "./sorting.module.css";
+import {RadioInput} from "../ui/radio-input/radio-input";
+import {Button} from "../ui/button/button";
+import {randomArr} from "../../utils/randomArr";
+import {Column} from "../ui/column/column";
+import {Direction} from "../../types/direction";
 
 export const SortingPage: React.FC = () => {
+  useEffect(() => {
+
+  }, [])
+  const initArray = useMemo(
+    () => randomArr()
+  , []);
+
+  const [visualArray, setVisualArray] = useState<Array<number>>(initArray);
+
+  const generateArr = () => {
+    setVisualArray(randomArr());
+  }
+
   return (
     <SolutionLayout title="Сортировка массива">
+      <section className={styles.section}>
+        <div className={styles.controlsContainer}>
+          <div className={styles.inputContainer}>
+            <RadioInput
+              label='Выбор'
+              checked={true}
+            />
+            <RadioInput
+              label='Пузырек'
+              checked={false}
+            />
+          </div>
+          <div className={styles.buttonContainer}>
 
+            <Button
+              type='button'
+              text='По возрастанию'
+              sorting={Direction.Ascending}
+            >
+            </Button>
+            <Button
+              type='button'
+              text='По убыванию'
+              sorting={Direction.Descending}
+            >
+            </Button>
+          </div>
+          <Button
+            type='button'
+            text='Новый массив'
+            onClick={generateArr}
+          >
+          </Button>
+        </div>
+        <ul className={styles.columns}>
+          {visualArray.map((item, index) => (
+            <li key={index}>
+              <Column
+                index={item}
+              >
+              </Column>
+            </li>
+          ))}
+        </ul>
+      </section>
     </SolutionLayout>
   );
 };
---
+++ src/utils/randomArr.ts
@@ -0,0 +1,8 @@
+export const randomArr = (minLen: number = 3, maxLen: number = 17, ): Array<number> => {
+	const result = [];
+	const length = Math.floor(Math.random() * (maxLen - minLen) + minLen);
+	for (let i = 0; i < length; i++) {
+		result.push(Math.floor(Math.random() * 100));
+	}
+	return result;
+}
