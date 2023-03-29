6c98500ed86fcfea6853e8fefd4176a1f21a7662
annasilina <92690224+annasilina@users.noreply.github.com>
2023-03-29 22:14:46 +0300

    added unit test for Button onClick callback

--- src/components/ui/button/button.test.js
+++ src/components/ui/button/button.test.js
@@ -2,33 +2,42 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import {Button} from './button';
+import {fireEvent, render, screen} from '@testing-library/react';
 
-describe('button', () => {
-	it('with text "Button" rendered correctly', () => {
+describe('<Button />', () => {
+	it('should render correctly with text "Test Button Text"', () => {
 		const element = renderer
-			.create(<Button text='Button'/>)
+			.create(<Button text='Test Button Text'/>)
 			.toJSON();
 		expect(element).toMatchSnapshot();
 	});
 
-	it('without text rendered correctly', () => {
+	it('should render correctly without text', () => {
 		const element = renderer
 			.create(<Button/>)
 			.toJSON();
 		expect(element).toMatchSnapshot();
 	});
 
-	it('disabled rendered correctly', () => {
+	it('should render correctly with disabled prop', () => {
 		const element = renderer
 			.create(<Button disabled={true}/>)
 			.toJSON();
 		expect(element).toMatchSnapshot();
 	});
 
-	it('with loader rendered correctly', () => {
+	it('should render correctly with loader', () => {
 		const element = renderer
 			.create(<Button isLoader={true}/>)
 			.toJSON();
 		expect(element).toMatchSnapshot();
+	});
+
+	it('callback function should be invoked after click', () => {
+		const onClickAction = jest.fn();
+		render(<Button onClick={onClickAction} text='button'/>);
+		const button = screen.getByText('button');
+		fireEvent.click(button);
+		expect(onClickAction).toHaveBeenCalledTimes(1);
 	});
 });
